55225da0d0be8f8281a480df095bc8dfac68d504
Amy Guan <amyguan@stanford.edu>
2023-10-27 13:05:40 -0700

    add logreg on pronouns only

--- logreg.py
+++ logreg.py
@@ -9,29 +9,61 @@ import sklearn
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+import re
 
-def tokenize(files, winner):
+def pronoun_tokenizer(text):
+    pronouns = "|".join(util.PRONOUNS)
+    # print(pronouns)
+    # print(re.findall(f"\b({pronouns})\b", text))
+    # return text
+    return re.findall(f"{pronouns}", text)
+
+
+def generate_lines_scores(file_path, winners):
+    filename = file_path.split('/')[-1]
+    winner = winners[filename].upper()
+    speaker_lines = util.split_speakers(file_path)
+
+    all_lines = []
+    ys = []
+    for speaker in speaker_lines:
+        lines = speaker_lines[speaker]
+
+        # for 2008, this means the speaker was not the moderator
+        if speaker in ["OBAMA", "MCCAIN", "BIDEN", "PALIN"]:
+            all_lines.extend(lines)
+            ys.extend([speaker == winner] * len(lines))
+    
+    return all_lines, ys
+
+
+def generate_pronouns_y(files, winners):
     for file_path in files:
-        filename = file_path.split('/')[-1]
-        winner = winners[filename].upper()
-        speaker_lines = util.split_speakers(file_path)
+        all_lines, ys = generate_lines_scores(file_path, winners)
+        
+    count_vect = CountVectorizer(tokenizer=pronoun_tokenizer)
+    count_matrix = count_vect.fit_transform(all_lines)
+    count_array = count_matrix.toarray()
+    X = pd.DataFrame(data=count_array, columns=count_vect.get_feature_names_out())
+    y = pd.DataFrame(ys)
 
-        all_lines = []
-        ys = []
-        for speaker in speaker_lines:
-            lines = speaker_lines[speaker]
+    return X, y
+    
 
-            # for 2008, this means the speaker was not the moderator
-            if speaker in ["OBAMA", "MCCAIN", "BIDEN", "PALIN"]:
-                all_lines.extend(lines)
-                ys.extend([speaker == winner] * len(lines))
-
+def generate_X_y(files, winners):
+    for file_path in files:
+        all_lines, ys = generate_lines_scores(file_path, winners)
+        
     count_vect = CountVectorizer(stop_words='english')
     count_matrix = count_vect.fit_transform(all_lines)
     count_array = count_matrix.toarray()
     X = pd.DataFrame(data=count_array, columns=count_vect.get_feature_names_out())
     y = pd.DataFrame(ys)
 
+    return X, y
+
+
+def logreg(X, y):
     # split data
     x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.25) # , random_state=0
 
@@ -42,7 +74,8 @@ def tokenize(files, winner):
     # predict
     # predictions = logisticRegr.predict(x_test)
     score = logisticRegr.score(x_test, y_test)
-    print(score)
+    print('logreg on words:', score)
+    
 
 
 
@@ -51,4 +84,13 @@ if __name__ == "__main__":
         winners = json.load(json_file)
 
     files = ['data/pres/09_26_2008.txt', 'data/pres/10_07_2008.txt', 'data/pres/10_15_2008.txt', 'data/vp/10_02_2008.txt']
-    speaker_items = tokenize(files, winners)
+    X, y = generate_X_y(files, winners)
+    logreg(X, y)
+
+    X, y = generate_pronouns_y(files, winners)
+    print(X[:20])
+    logreg(X, y)
+
+    # text = "hey I am we my me in.!they"
+    # pronouns = "|".join(util.PRONOUNS)
+    # print(re.findall(f"{pronouns}", text))
