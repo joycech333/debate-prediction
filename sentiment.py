4e2a937a6f8d5eaa4c86c41104b0cfc24ad41a3c
Sophie Andrews <sophie.j.andrews@gmail.com>
2023-12-05 12:48:18 -0800

    sentiment embeddings

--- sentiment.py
+++ sentiment.py
@@ -1,6 +1,11 @@
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+import numpy as np
 
 def process_debate(file_paths):
+    X = []
+    y = []
     for file_path in file_paths:
         print()
         print(file_path)
@@ -29,7 +34,14 @@ def process_debate(file_paths):
 
 
         for speaker in all_speakers:
+            # for 2008, this means the speaker was the moderator
+            if speaker not in ["OBAMA", "MCCAIN", "BIDEN", "PALIN"]:
+                continue
             sentences = all_speakers[speaker]
+
+            label = 0
+            if speaker == "OBAMA" or speaker == "BIDEN":
+                label = 1
 
             pos = 0
             neu = 0
@@ -42,6 +54,8 @@ def process_debate(file_paths):
                     temp.append(ss[k])
                 # understanding compound: https://towardsdatascience.com/social-media-sentiment-analysis-in-python-with-vader-no-training-required-4bc6a21e87b8
                 compound = temp[0]
+                X.append(compound)
+                y.append(label)
 
                 if compound <= -0.05:
                     neg += 1
@@ -50,14 +64,31 @@ def process_debate(file_paths):
                 else:
                     neu += 1
 
-            # for 2008, this means the speaker was the moderator
-            if speaker not in ["OBAMA", "MCCAIN", "BIDEN", "PALIN"]:
-                continue
+            
             print(speaker, "pos:" + str(pos), "neu:" + str(neu), "neg:" + str(neg))
+
+    return X, y
 
 
 # Specify the path to your debate text file
-file_paths = ['data/pres/09_26_2008.txt', 'data/pres/10_07_2008.txt', 'data/pres/10_15_2008.txt', 'data/vp/10_02_2008.txt']
+file_paths = ['scraped-data/transcripts/September_26_2008.txt', 'scraped-data/transcripts/October_07_2008.txt', 'scraped-data/transcripts/October_15_2008.txt', 'scraped-data/transcripts/October_02_2008.txt']
 
 # Process the debate file
-process_debate(file_paths)
+X, y = process_debate(file_paths)
+print(len(X))
+print(len(y))
+
+# split data
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0) # , random_state=0
+
+x_train= np.reshape(x_train, (-1, 1))
+x_test= np.reshape(x_test, (-1, 1))
+
+# fit regression
+logisticRegr = LogisticRegression()
+logisticRegr.fit(x_train, y_train)
+
+# predict
+# predictions = logisticRegr.predict(x_test)
+score = logisticRegr.score(x_test, y_test)
+print('logreg on words:', score)
